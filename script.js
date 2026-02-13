2faaa182c52a4d190d9cdffa10d50e52eee499d4
Davi <davilimasoares1@outlook.com>
2026-02-12 23:13:23 -0300

    I added a function to player 1 play a round

--- script.js
+++ script.js
@@ -10,9 +10,24 @@ function createPlayer(mark, name) {
 
 const play = (() => {
     const add = (mark, coord) => {
-        table[coord] = mark
+        table[coord] = mark;
     }
-})
+});
+
+function roundForPlayer1(player1, mark1) {
+    let coord = prompt('Where do you want to pu your mark? 0-8');
+    table.forEach(element => {
+        if (parseInt(coord) === element.indexOf()) {
+            console.log('This coordinate is already occupied, please select another');
+            round(player1, mark1);
+        }
+    });
+
+    play.add(mark1, coord);
+    console.log(table);
+}
+
+
 
 function startGame() {
     nameInput1 = prompt("What's the name for player 1");
@@ -29,4 +44,6 @@ function startGame() {
 
     const player1 = createPlayer(markInput1, nameInput1);
     const player2 = createPlayer(markInput2, nameInput2);
+
+    
 }
