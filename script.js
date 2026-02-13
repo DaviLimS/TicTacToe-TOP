5c7227a5d215492f8743f90bb0a3b0057b8b2ac8
Davi <davilimasoares1@outlook.com>
2026-02-13 10:53:08 -0300

    I also improved the diagonal check

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ function startGame() {
             [table[0], table[1], table[2]].includes(player1.mark) ? console.log(`${player1.name} Won!!`) : console.log(`${player2.name} Won!!`);
         }
 
-        if (table[0] === table[4] === table[8] || table[2] === table[4] || table[6]) {
+        if (table[0] != '' && table[0] === table[4] && table[4] === table[8] || table[2] != '' && table[2] === table[4] && table[4] === table[6]) {
             [table[0], table[2]].includes(player1.mark) ? console.log(`${player1.name} Won!!`) : console.log(`$${player2.name} Won!!`);
         }
     }
