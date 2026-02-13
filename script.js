6fcc63cc3944db4c64d7e3f8fcac43dcfb243a8a
Davi <davilimasoares1@outlook.com>
2026-02-12 22:59:13 -0300

    Now, it's possible to create two players

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 let table = ['', '', '', '', '', '', '', '', ''];
-let nameInput;
-let markInput;
-
-function startGame() {
-    nameInput = prompt("What's your nick?");
-    markInput = prompt("Do you prefer X or O?");
-    if(markInput === 'x' || 'X' || 'o' || 'O') {
-        markInput === 'x' || 'X' ? 'X' : 'O';
-    }
-    else {
-        console.log('Select X or O');
-        startGame();
-    }
-}
+let nameInput1;
+let nameInput2;
+let markInput1;
+let markInput2;
 
 function createPlayer(mark, name) {
     return { mark, name }
 }
-
-const player1 = createPlayer();
 
 const play = (() => {
     const add = (mark, coord) => {
         table[coord] = mark
     }
 })
+
+function startGame() {
+    nameInput1 = prompt("What's the name for player 1");
+    nameInput2 = prompt("What's the name for player 2");
+    markInput1 = prompt("Do you prefer X or O?");
+    if(markInput1 === 'x' || 'X' || 'o' || 'O') {
+        markInput1 === 'x' || 'X' ? 'X' : 'O';
+        markInput1 === 'X' || 'O' ? markInput2 = 'O' : markInput2 = 'X';
+    }
+    else {
+        console.log('Select X or O');
+        startGame();
+    }
+
+    const player1 = createPlayer(markInput1, nameInput1);
+    const player2 = createPlayer(markInput2, nameInput2);
+}
