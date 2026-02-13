808ecfed59e8a7a871c9b8bb495bd693cf313051
Davi <davilimasoares1@outlook.com>
2026-02-13 11:01:13 -0300

    I corrected the table displayer

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function roundForPlayer1(player1, mark1) {
 
     play.add(mark1, coord);
     for (let index = 0; index < table.length; index++) {
-        index === 3 || index === 6 ? console.log(`\ntable[index] `) : console.log(`${table[index]} `);
+        index === 3 || index === 6 ? console.log(`\n${table[index]} `) : console.log(`${table[index]} `);
     }
 }
 
