eca88373c7541ccc75d810368a358c604e27e9c1
Davi <davilimasoares1@outlook.com>
2026-02-12 22:54:11 -0300

    I started to create the if and the way to select a name and a marker

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 let table = ['', '', '', '', '', '', '', '', ''];
+let nameInput;
+let markInput;
 
-function player(mark, name) {
+function startGame() {
+    nameInput = prompt("What's your nick?");
+    markInput = prompt("Do you prefer X or O?");
+    if(markInput === 'x' || 'X' || 'o' || 'O') {
+        markInput === 'x' || 'X' ? 'X' : 'O';
+    }
+    else {
+        console.log('Select X or O');
+        startGame();
+    }
+}
+
+function createPlayer(mark, name) {
     return { mark, name }
 }
 
-
+const player1 = createPlayer();
 
 const play = (() => {
     const add = (mark, coord) => {
