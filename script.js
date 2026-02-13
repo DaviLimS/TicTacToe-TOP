c09ccb4523220b6cd5060315b181a9e3b2d71542
Davi <davilimasoares1@outlook.com>
2026-02-13 10:25:14 -0300

    I added a conditional and ternary operator to check if a diagonal were completed

--- script.js
+++ script.js
@@ -71,5 +71,8 @@ function startGame() {
         if (table[0] === table[3] === table[6] || table[1] === table[4] === table[7] || table[2] === table[5] === table[8]) {
             [table[0], table[1], table[2]].includes(player1.mark) ? console.log(`${player1.name} Won!!`) : console.log(`${player2.name} Won!!`);
         }
+        if (table[0] === table[4] === table[8] || table[2] === table[4] || table[6]) {
+            [table[0], table[2]].includes(player1.mark) ? console.log(`${player1.name} Won!!`) : console.log(`$${player2.name} Won!!`);
+        }
     }
 }
