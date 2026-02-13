4e1052bef325db493bc9cbeb86a0c48f46e6afb1
Davi <davilimasoares1@outlook.com>
2026-02-13 10:51:38 -0300

    Now the conditional for horizontal lines completition is working better

--- script.js
+++ script.js
@@ -70,9 +70,11 @@ function startGame() {
         if (table[0] != '' && table[0] === table[1] && table[1] === table[2] || table[3] != '' && table[3] === table[4] && table[4] === table[5] && table[5] === table[6] && table[6] === table[7] && table[7] === table[8]) {
             [table[0], table[3], table[6]].includes(player1.mark) ? console.log(`${player1.name} Won!!`) : console.log(`${player2.name} Won!!`);
         }
-        if (table[0] === table[3] === table[6] || table[1] === table[4] === table[7] || table[2] === table[5] === table[8]) {
+
+        if (table[0] != '' && table[0] === table[3] && table[3] === table[6] || table[1] != '' && table[1] === table[4] && table[4] === table[7] || table[2] != '' && table[2] === table[5] && table[5] === table[8]) {
             [table[0], table[1], table[2]].includes(player1.mark) ? console.log(`${player1.name} Won!!`) : console.log(`${player2.name} Won!!`);
         }
+
         if (table[0] === table[4] === table[8] || table[2] === table[4] || table[6]) {
             [table[0], table[2]].includes(player1.mark) ? console.log(`${player1.name} Won!!`) : console.log(`$${player2.name} Won!!`);
         }
